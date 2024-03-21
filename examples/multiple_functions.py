81c5c2fd0a41ce2f87ac9bd2bedc3a30558b5d10
zimolab <zimolab@aliyun.com>
2024-03-21 22:31:36 +0800

    feat(app): add more examples

--- examples/multiple_functions.py
+++ examples/multiple_functions.py
@@ -1,4 +1,6 @@
 from examples.parameter_types import supported_types
+from examples.customize_widget_1 import customize_widget_1
+from examples.customize_widget_2 import customize_widget_2
 from examples.print_and_logging import print_logging_demo
 from examples.interact_by_popups import interact_with_popups
 from examples.get_started import user_function
@@ -16,6 +18,46 @@ if __name__ == "__main__":
     gui_adapter = GUIAdapter()
     gui_adapter.selection_window_config.icon_mode = False
     gui_adapter.add(supported_types)
+
+    widgets_config = {
+        "name": {
+            "type": "LineEdit",
+            "label": "Account",
+            "docstring": "input your name here",
+            "show_label": True,
+            "show_docstring": True,
+            "placeholder": "input account here",
+        },
+        "password": {
+            "type": "LineEdit",
+            "label": "Password",
+            "echo_mode": "Password",
+            "show_label": True,
+            "show_docstring": True,
+            "docstring": "input your password",
+        },
+        "age": {
+            "type": "IntSpinBox",
+            "label": "Your age",
+            "docstring": "input your age here",
+            "show_label": True,
+            "show_docstring": True,
+            "min_value": 0,
+            "max_value": 100,
+        },
+        "intro": {
+            "type": "PlainTextEdit",
+            "docstring": "this text will not be seen, cause show_docstring is set to False",
+            "show_docstring": False,
+        },
+    }
+
+    gui_adapter.add(
+        customize_widget_1,
+        display_document="customize parameter widgets by widget_configs",
+        widgets_config=widgets_config,
+    )
+    gui_adapter.add(customize_widget_2)
     gui_adapter.add(
         print_logging_demo,
         display_name="Print and Logging Demo",
