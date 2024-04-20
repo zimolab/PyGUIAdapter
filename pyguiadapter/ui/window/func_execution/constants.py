4f5389af8257262967bb6f374a19ade2bbb710b6
zimolab <zimolab@aliyun.com>
2024-04-20 21:30:31 +0800

    refactor(main): change some texts

--- pyguiadapter/ui/window/func_execution/constants.py
+++ pyguiadapter/ui/window/func_execution/constants.py
@@ -2,13 +2,13 @@ from typing import Optional, List, Tuple
 
 from PyQt6.QtWidgets import QApplication
 
-FUNC_RESULT_MSG = QApplication.tr("function result: {}")
+FUNC_RESULT_MSG = QApplication.tr("{}")
 FUNC_RESULT_DIALOG_TITLE = QApplication.tr("Function Result")
-FUNC_START_MSG = QApplication.tr("function execution started")
-FUNC_FINISH_MSG = QApplication.tr("function execution finished")
-FUNC_ERROR_MSG = QApplication.tr("function execution error: {}")
+FUNC_START_MSG = QApplication.tr("Execution Started")
+FUNC_FINISH_MSG = QApplication.tr("Execution Finished")
+FUNC_ERROR_MSG = QApplication.tr("Error: {}")
 FUNC_ERROR_DIALOG_TITLE = QApplication.tr("Function Execution Error")
-BUSY_MSG = QApplication.tr("A function is already running!")
+BUSY_MSG = QApplication.tr("A function is already executing!")
 BUSY_DIALOG_TITLE = QApplication.tr("Busy")
 
 DOCK_SIZES = (460, 460)
