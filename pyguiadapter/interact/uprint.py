bfbd77c8ec3881858c5d5f5e6e5a52b3e3764224
zimolab <zimolab@aliyun.com>
2024-04-20 21:20:11 +0800

    feat(main): add uprint.uprint_image()
    now it is to display images in output area

--- pyguiadapter/interact/uprint.py
+++ pyguiadapter/interact/uprint.py
@@ -1,3 +1,5 @@
+import os.path
+
 from PyQt6.QtCore import QObject, pyqtSignal
 
 
@@ -25,3 +27,14 @@ def remove_print_destination(func: callable):
 
 def uprint(*args, sep=" ", end="\n", html: bool = False):
     __uprint.print(*args, sep=sep, end=end, html=html)
+
+
+def uprint_image(image_path: str, alt_text: str = "", blank_lines_around: bool = False):
+    if blank_lines_around:
+        outer_tag = "<br>{}<br>"
+    else:
+        outer_tag = "{}"
+
+    img_tag = f"<img src='{os.path.abspath(image_path)}' alt='{alt_text}' />"
+
+    uprint(outer_tag.format(img_tag), html=True)
