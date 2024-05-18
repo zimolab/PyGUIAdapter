c9125d93e957ecb8766f3b85758627e1998da78c
zimolab <zimolab@aliyun.com>
2024-05-18 12:52:46 +0800

    feat(progress-bar-feature): add update progress demo

--- examples/progressbar_demo1.py
+++ examples/progressbar_demo1.py
@@ -1,9 +1,29 @@
+import time
 from pyguiadapter import GUIAdapter
 from pyguiadapter.progressbar_config import ProgressBarConfig
+from pyguiadapter.interact.uprint import (
+    update_progress,
+    update_progressbar_config,
+    uprint,
+)
 
 
-def progressbar_demo1() -> int:
-    pass
+def progressbar_demo1(min_value: int, max_value: int = 10, delay: float = 1.0):
+    update_progressbar_config(
+        min_value=min_value,
+        max_value=max_value,
+        progress_text_format="%v/%m (%p%)",
+        show_progressbar_info=True,
+    )
+    for i in range(min_value, max_value + 1):
+        uprint(f"processing: {i}")
+        update_progress(
+            i, progress_info=f"processing item {i}... \nthis may take some time"
+        )
+        time.sleep(delay)
+    uprint("process done")
+    # reset progress manually
+    update_progress(min_value, progress_info="")
 
 
 if __name__ == "__main__":
@@ -14,7 +34,7 @@ if __name__ == "__main__":
         inverted_appearance=False,
         show_progress_text=True,
         progress_text_centered=True,
-        progress_text_format="task %v in task %m",
+        progress_text_format="%v / %m (%p%)",
         show_progressbar_info=False,
     )
     gui_adapter.add(
