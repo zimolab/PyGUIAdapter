183a5ca0e89c28a4df3c527f30e16ce02e07e42f
zimolab <zimolab@aliyun.com>
2024-09-14 00:23:48 +0800

    feature(v2): add widget example file

--- examples/widgets/float_example.py
+++ examples/widgets/float_example.py
@@ -1,11 +1,53 @@
 from pyguiadapter.adapter import GUIAdapter
+from pyguiadapter.adapter.ucontext import uprint
+from pyguiadapter.widgets import FloatSpinBoxConfig
 
 
-def float_example(float_arg1: float, float_arg2: float, float_arg3: float):
-    pass
+def float_example(float_arg1: float, float_arg2: float, float_arg3: float = 3.14):
+    """
+    example for **float** and **FloatSpinBox**
+
+    @param float_arg1: this parameter is configured in docstring , see `@params ... @end` block below
+    @param float_arg2: this parameter is configured with `run()` via a FloatSpinBoxConfig object
+    @param float_arg3: this parameter is configured with `run()` via a dict instance
+    @return:
+
+    @params
+    [float_arg1]
+    default_value = 1.0
+    max_value = 100.0
+    step = 2.0
+    decimals = 3
+    prefix = "r: "
+    suffix = "%"
+
+    @end
+
+    """
+    uprint("float_arg1", float_arg1)
+    uprint("float_arg2", float_arg2)
+    uprint("float_arg3", float_arg3)
+    return float_arg1 + float_arg2 + float_arg3
 
 
 if __name__ == "__main__":
     adapter = GUIAdapter()
-    adapter.add(float_example)
+
+    float_arg2_config = FloatSpinBoxConfig(
+        default_value=-0.5, max_value=1.0, step=0.000005, decimals=5, prefix="R: "
+    )
+
+    adapter.add(
+        float_example,
+        widget_configs={
+            "float_arg2": float_arg2_config,
+            "float_arg3": {
+                # this will override the default_value in the function signature
+                "default_value": 0.5,
+                "max_value": 2.0,
+                "step": 0.00001,
+                "decimals": 5,
+            },
+        },
+    )
     adapter.run()
