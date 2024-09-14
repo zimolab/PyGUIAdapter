c64987827560829417f6b3d3c0eb0c4b5655724a
zimolab <zimolab@aliyun.com>
2024-09-14 19:38:41 +0800

    feature(v2): add example for bool widget

--- examples/widgets/bool_example.py
+++ examples/widgets/bool_example.py
@@ -1,11 +1,59 @@
 from pyguiadapter.adapter import GUIAdapter
+from pyguiadapter.adapter.ucontext import uprint
+from pyguiadapter.widgets import BoolBoxConfig
 
 
-def bool_example(bool_arg1: bool, bool_arg2: bool, bool_arg3: bool):
-    pass
+def bool_example(
+    bool_arg1: bool = False, bool_arg2: bool = True, bool_arg3: bool = False
+):
+    """
+    example for type **bool** and **BoolBox** widget
+
+    @param bool_arg1: this parameter will be configured in docstring
+    @param bool_arg2: this parameter will be configured with **adapter.run()** via a BoolBoxConfig object
+    @param bool_arg3: this parameter will be configured with **adapter.run()** via a dict
+    @return:
+
+    @params
+    [bool_arg1]
+    # this will override the default value defined in the function signature
+    default_value = true
+    true_text = "On"
+    false_text = "Off"
+    true_icon = "fa.toggle-on"
+    false_icon = "fa.toggle-off"
+    vertical = false
+    @end
+    """
+    uprint(bool_arg1, bool_arg2, bool_arg3)
+    return bool_arg1, bool_arg2, bool_arg3
 
 
 if __name__ == "__main__":
+
+    bool_arg2_conf = {
+        # this will override the default value defined in the function signature
+        "default_value": True,
+        "true_text": "Enable",
+        "false_text": "Disable",
+        "true_icon": "fa.toggle-on",
+        "false_icon": "fa.toggle-off",
+        "vertical": True,
+    }
+
+    bool_arg3_conf = BoolBoxConfig(
+        # this will override the default value defined in the function signature
+        default_value=True,
+        true_text="true",
+        false_text="false",
+        true_icon="fa.toggle-on",
+        false_icon="fa.toggle-off",
+        vertical=False,
+    )
+
     adapter = GUIAdapter()
-    adapter.add(bool_example)
+    adapter.add(
+        bool_example,
+        widget_configs={"bool_arg2": bool_arg2_conf, "bool_arg3": bool_arg3_conf},
+    )
     adapter.run()
