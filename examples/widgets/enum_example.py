9122156c3efc7cdc4d25d163c5caf9256cd0b7df
zimolab <zimolab@aliyun.com>
2024-09-16 00:22:55 +0800

    feature(v2): add example code

--- examples/widgets/enum_example.py
+++ examples/widgets/enum_example.py
@@ -1,6 +1,7 @@
 from enum import Enum
 from pyguiadapter.adapter import GUIAdapter
 from pyguiadapter.adapter.ulogging import uprint
+from pyguiadapter.widgets import EnumSelectConfig
 
 
 class Weekday(Enum):
@@ -20,11 +21,36 @@ class Color(Enum):
 
 
 def enums_example(day_enums: Weekday, color_enums: Color = Color.GREEN):
+    """
+    example for type **enum** and **EnumSelect**
+    @param day_enums: Enums for week days
+    @param color_enums:  Enums for RGB colors
+    @return:
+
+    @params
+    [day_enums]
+    # this will override the default value defined in the function signature
+    default_value = "MONDAY"
+    icons = {"MONDAY"="mdi6.numeric-1-circle", "TUESDAY"="mdi6.numeric-2-circle"}
+
+    @end
+
+    """
     uprint(day_enums)
     uprint(color_enums)
 
 
 if __name__ == "__main__":
+    color_enums_conf = EnumSelectConfig(
+        # this will override the default value defined in the function signature
+        default_value=Color.BLUE,
+        icons={
+            "RED": ("mdi.invert-colors", {"color": "red"}),
+            "GREEN": ("mdi.invert-colors", {"color": "green"}),
+            "BLUE": ("mdi.invert-colors", {"color": "blue"}),
+        },
+        icon_size=(24, 24),
+    )
     adapter = GUIAdapter()
-    adapter.add(enums_example)
+    adapter.add(enums_example, widget_configs={"color_enums": color_enums_conf})
     adapter.run()
