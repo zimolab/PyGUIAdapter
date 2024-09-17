e0a6bdaab66d532f13b7a883890dc7662be8ab52
zimolab <zimolab@aliyun.com>
2024-09-17 21:36:39 +0800

    feature(v2): add example code

--- examples/widgets/time_example.py
+++ examples/widgets/time_example.py
@@ -1,16 +1,36 @@
 from datetime import time
 
 from pyguiadapter.adapter import GUIAdapter
+from pyguiadapter.adapter.ucontext import uprint
+from pyguiadapter.widgets import TimeEditConfig
 
 
 def time_example(arg1: time, arg2: time, arg3: time):
     """
     example for type **time** and **TimeEdit** widget
     """
-    pass
+    uprint("arg1:", arg1)
+    uprint("arg2:", arg2)
+    uprint("arg3:", arg3)
 
 
 if __name__ == "__main__":
+    arg1_conf = TimeEditConfig(
+        default_value=time(hour=12, minute=30),
+        max_time=time(hour=23, minute=59),
+        min_time=time(hour=0, minute=0),
+    )
+    arg2_conf = TimeEditConfig(
+        default_value=time(hour=20, minute=30),
+        max_time=time(hour=23, minute=59),
+        min_time=time(hour=12, minute=0),
+    )
     adapter = GUIAdapter()
-    adapter.add(time_example)
+    adapter.add(
+        time_example,
+        widget_configs={
+            "arg1": arg1_conf,
+            "arg2": arg2_conf,
+        },
+    )
     adapter.run()
