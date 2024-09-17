125a4d79b73d7b84da0a4af1897d077e311a3957
zimolab <zimolab@aliyun.com>
2024-09-17 21:33:02 +0800

    feature(v2): add example code

--- examples/widgets/date_example.py
+++ examples/widgets/date_example.py
@@ -1,16 +1,36 @@
 from datetime import date
 
 from pyguiadapter.adapter import GUIAdapter
+from pyguiadapter.adapter.ucontext import uprint
+from pyguiadapter.widgets import DateEditConfig
 
 
 def date_example(arg1: date, arg2: date, arg3: date):
     """
-    example for type **date** and **DateEdit** widget
+    example for type **date** and DateEdit widget
     """
-    pass
+    uprint("arg1", arg1)
+    uprint("arg2", arg2)
+    uprint("arg3", arg3)
 
 
 if __name__ == "__main__":
+    arg1_conf = DateEditConfig(
+        min_date=date(2023, 1, 1),
+        max_date=date(2023, 12, 31),
+    )
+    arg3_conf = DateEditConfig(
+        default_value=date(2023, 6, 1),
+        min_date=date(2023, 1, 1),
+        max_date=date(2023, 12, 31),
+        calendar_popup=True,
+    )
     adapter = GUIAdapter()
-    adapter.add(date_example)
+    adapter.add(
+        date_example,
+        widget_configs={
+            "arg1": arg1_conf,
+            "arg3": arg3_conf,
+        },
+    )
     adapter.run()
