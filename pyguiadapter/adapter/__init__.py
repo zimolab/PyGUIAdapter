2fdd16d2e5419e9691991ef909dc990767658415
zimolab <zimolab@aliyun.com>
2024-09-22 18:18:45 +0800

    refactor(v2): add import

--- pyguiadapter/adapter/__init__.py
+++ pyguiadapter/adapter/__init__.py
@@ -1,5 +1,10 @@
 from .adapter import GUIAdapter
 from . import uoutput
+from .udialog import BaseCustomDialog
 
 # noinspection SpellCheckingInspection
-__all__ = ["GUIAdapter", "uoutput"]
+__all__ = [
+    "GUIAdapter",
+    "BaseCustomDialog",
+    "uoutput",
+]
