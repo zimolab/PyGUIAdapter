db7a3f30cea13618ea2bfc1a1c22dc9ee109b823
zimolab <zimolab@aliyun.com>
2024-09-22 21:06:11 +0800

    feature(v2): add example code

---
+++ examples/adapter/cancellable_function.py
@@ -0,0 +1,31 @@
+import time
+
+from pyguiadapter.adapter import GUIAdapter
+from pyguiadapter.adapter.ucontext import uprint, is_function_cancelled
+
+
+def cancellable_function_example(target: int = 10, delay_per_iter: float = 0.5):
+    """
+    @params
+    [target]
+    min_value = 1
+
+    [delay_per_iter]
+    min_value = 0.001
+    step = 0.5
+    decimals = 3
+
+    @end
+    """
+    for i in range(target):
+        if is_function_cancelled():
+            break
+        uprint("process: ", i)
+        time.sleep(delay_per_iter)
+    uprint("done!")
+
+
+if __name__ == "__main__":
+    adapter = GUIAdapter()
+    adapter.add(cancellable_function_example, cancelable=True)
+    adapter.run()
