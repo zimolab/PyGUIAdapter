9eae833171b1054e42a9cc7a9861a72e2d5d9cbd
zimolab <zimolab@aliyun.com>
2024-09-23 18:52:16 +0800

    feature(v2): add example code

--- examples/adapter/progressbar_example.py
+++ examples/adapter/progressbar_example.py
@@ -1,19 +1,40 @@
 import time
 
-from pyguiadapter.adapter import GUIAdapter
+from pyguiadapter.adapter import GUIAdapter, udialog
 from pyguiadapter.adapter import uprogress
 from pyguiadapter.adapter.ucontext import is_function_cancelled, uprint
 
 
-def progressbar_example(target: int = 100, delay: float = 0.5):
-    uprogress.show_progressbar(0, 100, message_visible=True)
-    for i in range(target):
+def progressbar_example(total: int = 100, delay: float = 0.5):
+    """
+    example for **progressbar** and **cancellable function**
+
+    @param total: how many task will be processed
+    @param delay: how much time will be consumed per task
+    @return:
+    """
+    uprogress.show_progressbar(0, total, message_visible=True)
+    cancelled = False
+    task_processed = 0
+    for i in range(total):
         if is_function_cancelled():
             uprint("Cancelled!")
+            cancelled = True
             break
-        uprint(f"[info] {i}")
-        uprogress.update_progress(i, info=f"progress: <b>{i}%</b>")
+        task_processed = i + 1
+        uprint(f"[Processed] {task_processed}")
+        uprogress.update_progress(
+            task_processed, info=f"[{task_processed}/{total}] please wait..."
+        )
         time.sleep(delay)
+    if cancelled:
+        udialog.show_warning_dialog(
+            f"{task_processed} task(s) processed!", title="Cancelled"
+        )
+    else:
+        udialog.show_info_dialog(
+            f"{task_processed} task(s) processed!", title="Completed"
+        )
     uprogress.hide_progressbar()
 
 
