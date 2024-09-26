bc3b8bb58c97f593f9b03b84c89bdbda35c143ad
zimolab <zimolab@aliyun.com>
2024-09-26 22:02:14 +0800

    refactor(v2): clean up useless code

--- pyguiadapter/windows/__init__.py
+++ pyguiadapter/windows/__init__.py
@@ -1,15 +1,5 @@
 from ._docbrowser import DocumentBrowserConfig
 
-# from .fnexec import (
-#     FnExecuteWindow,
-#     FnExecuteWindowConfig,
-#     WidgetTexts,
-#     MessageTexts,
-#     OutputBrowserConfig,
-#     ProgressBarConfig,
-# )
-# from .fnselect import FnSelectWindow, FnSelectWindowConfig
-
 from ._docbrowser import DocumentBrowserConfig
 
 __all__ = [
--- pyguiadapter/windows/fnexec/__init__.py
+++ pyguiadapter/windows/fnexec/__init__.py
@@ -1,21 +1,3 @@
-# from ._base import (
-#     FnExecuteWindowConfig,
-#     ProgressBarConfig,
-#     WidgetTexts,
-#     MessageTexts,
-# )
-# from ._outputbrowser import OutputBrowserConfig
-# from ._window import FnExecuteWindow
-#
-# __all__ = [
-#     "FnExecuteWindow",
-#     "FnExecuteWindowConfig",
-#     "ProgressBarConfig",
-#     "OutputBrowserConfig",
-#     "WidgetTexts",
-#     "MessageTexts",
-# ]
-
 from ._base import FnExecuteWindowConfig, WidgetTexts, MessageTexts
 from ._progressbar import ProgressBarConfig
 from ._outputbrowser import OutputBrowserConfig
