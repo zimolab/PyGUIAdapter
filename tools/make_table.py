b46bd882ce27bd3dc6fdd0fa19d1ead858aeeec5
zimolab <zimolab@aliyun.com>
2024-10-02 03:05:25 +0800

    docs(v2): improve docs

---
+++ tools/make_table.py
@@ -0,0 +1,8 @@
+from tools.gen_widget_doc import dataclass2table
+
+from pyguiadapter.windows.fnselect import FnSelectWindowConfig
+from pyguiadapter.windows import DocumentBrowserConfig
+
+if __name__ == "__main__":
+    # print(dataclass2table(FnSelectWindowConfig))
+    print(dataclass2table(DocumentBrowserConfig))
