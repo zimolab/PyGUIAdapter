4b17993021be819ba7b41f0e8bb0744486e9df36
zimolab <zimolab@aliyun.com>
2024-10-02 13:33:52 +0800

    refactor(v2): update example code

---
+++ examples/adapter/output_logger_example.py
@@ -0,0 +1,33 @@
+from pyguiadapter.adapter import GUIAdapter
+from pyguiadapter.adapter.uoutput import Logger, LoggerConfig
+
+logger = Logger(
+    confing=LoggerConfig(
+        info_color="green",
+        debug_color="blue",
+        warning_color="yellow",
+        critical_color="red",
+        fatal_color="magenta",
+    )
+)
+
+
+def output_log_msg(
+    info_msg: str = "info message",
+    debug_msg: str = "debug message",
+    warning_msg: str = "warning message",
+    critical_msg: str = "critical message",
+    fatal_msg: str = "fatal message",
+):
+
+    logger.info(info_msg)
+    logger.debug(debug_msg)
+    logger.warning(warning_msg)
+    logger.critical(critical_msg)
+    logger.fatal(fatal_msg)
+
+
+if __name__ == "__main__":
+    adapter = GUIAdapter()
+    adapter.add(output_log_msg)
+    adapter.run()
---
+++ examples/uoutput_example.py
@@ -0,0 +1,22 @@
+from pyguiadapter.adapter import GUIAdapter, uoutput
+from pyguiadapter.adapter.ucontext import uprint
+
+
+def output_log_msg(
+    info_msg: str = "info message",
+    debug_msg: str = "debug message",
+    warning_msg: str = "warning message",
+    critical_msg: str = "critical message",
+    fatal_msg: str = "fatal message",
+):
+    uoutput.info(info_msg)
+    uoutput.debug(debug_msg)
+    uoutput.warning(warning_msg)
+    uoutput.critical(critical_msg)
+    uoutput.fatal(fatal_msg)
+
+
+if __name__ == "__main__":
+    adapter = GUIAdapter()
+    adapter.add(output_log_msg, document_format="html")
+    adapter.run()
