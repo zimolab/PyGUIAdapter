9df252fc1d6c513f41924b6de564bd38755a9f8d
zimolab <zimolab@aliyun.com>
2024-10-09 21:18:49 +0800

    refactor: add import

--- pyguiadapter/windows/fnexec/__init__.py
+++ pyguiadapter/windows/fnexec/__init__.py
@@ -10,11 +10,12 @@ from ._base import (
     AllDockWidgetAreas,
 )
 from ._output_area import ProgressBarConfig, OutputBrowserConfig
-from ._window import FnExecuteWindow
+from ._window import FnExecuteWindow, BaseFnExecuteWindow
 
 __all__ = [
     "FnExecuteWindowConfig",
     "FnExecuteWindow",
+    "BaseFnExecuteWindow",
     "ProgressBarConfig",
     "OutputBrowserConfig",
     "DockWidgetArea",
