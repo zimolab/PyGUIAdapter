c55448ebadae20bc5382e8a405fd93bccad43c3a
zimolab <zimolab@aliyun.com>
2024-10-15 22:38:47 +0800

    feature: add docstrings

--- pyguiadapter/toolbar.py
+++ pyguiadapter/toolbar.py
@@ -10,6 +10,7 @@ TopToolBarArea = ToolBarArea.TopToolBarArea
 BottomToolBarArea = ToolBarArea.BottomToolBarArea
 LeftToolBarArea = ToolBarArea.LeftToolBarArea
 RightToolBarArea = ToolBarArea.RightToolBarArea
+
 ToolBarAreas = Union[ToolBarArea, int]
 AllToolBarAreas = Qt.AllToolBarAreas
 
@@ -24,12 +25,25 @@ ToolButtonTextOnly = ToolButtonStyle.ToolButtonTextOnly
 @dataclasses.dataclass(frozen=True)
 class ToolBar(object):
     actions: List[Union[Action, Separator]]
+    """要添加到工具栏中的动作（`Action`）或分隔符（`Separator`）列表。在工具栏中，动作`Action`以工具栏按钮的形式出现。"""
+
     moveable: bool = True
+    """工具栏是否可以移动。"""
+
     floatable: bool = True
+    """工具栏是否可以漂浮在主窗口之外。"""
+
     icon_size: Union[int, Tuple[int, int], QSize, None] = None
+    """工具栏按钮的图标大小，"""
+
     initial_area: Optional[ToolBarArea] = None
+    """工具栏在窗口上的初始位置。"""
+
     allowed_areas: Optional[ToolBarAreas] = None
+    """窗口上允许工具栏停靠的位置。"""
+
     button_style: Optional[ToolButtonStyle] = None
+    """工具栏按的样式。"""
 
     def remove_action(self, action: Union[str, Action, Separator]):
         if isinstance(action, str):
