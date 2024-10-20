f7219afd443f2ec5577dfbef07fa393ca3fa80af
zimolab <zimolab@aliyun.com>
2024-10-20 17:50:43 +0800

    refactor: add docstrings

--- pyguiadapter/adapter/ubeep.py
+++ pyguiadapter/adapter/ubeep.py
@@ -1,5 +1,19 @@
+"""
+@Time    : 2024.10.20
+@File    : ubeep.py
+@Author  : zimolab
+@Project : PyGUIAdapter
+@Desc    : 提供了发出蜂鸣声的功能
+"""
+
 from qtpy.QtWidgets import QApplication
 
 
-def beep():
+def beep() -> None:
+    """
+    发出蜂鸣声。
+
+    Returns:
+        无返回值
+    """
     QApplication.beep()
