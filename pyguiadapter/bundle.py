14ebbff673f9160a118f35b83fab9526c9c07add
zimolab <zimolab@aliyun.com>
2024-10-20 17:46:01 +0800

    refactor: add module docstring

--- pyguiadapter/bundle.py
+++ pyguiadapter/bundle.py
@@ -1,3 +1,11 @@
+"""
+@Time    : 2024.10.20
+@File    : bundle.py
+@Author  : zimolab
+@Project : PyGUIAdapter
+@Desc    : 定义了FnBundle类，用于封装函数信息、参数配置、窗口配置、窗口事件监听器、窗口工具栏、窗口菜单等信息。方便传参。仅限于内部使用。
+"""
+
 import dataclasses
 from typing import Type, Tuple, Dict, Callable, Any, Optional, List, Union
 
