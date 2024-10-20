9462c6a96b214b84e35bdd3706edcd4a4bebe20e
zimolab <zimolab@aliyun.com>
2024-10-20 17:48:49 +0800

    refactor: add docstrings

--- pyguiadapter/utils/filedialog.py
+++ pyguiadapter/utils/filedialog.py
@@ -1,3 +1,11 @@
+"""
+@Time    : 2024.10.20
+@File    : filedialog.py
+@Author  : zimolab
+@Project : PyGUIAdapter
+@Desc    : 提供文件选择对话框的相关工具函数
+"""
+
 from typing import List, Optional
 
 from qtpy import compat
@@ -9,9 +17,20 @@ def get_existing_directory(
     parent: Optional[QWidget] = None,
     title: str = "Open Directory",
     start_dir: str = "",
-) -> str:
-    return compat.getexistingdirectory(parent, title, start_dir)
-    # return QFileDialog.getExistingDirectory(parent, title, start_dir)
+) -> Optional[str]:
+    """
+    弹出选择目录对话框，获取已存在的目录路径
+
+    Args:
+        parent: 父窗口
+        title:  对话框标题
+        start_dir: 起始目录
+
+    Returns:
+        用户选择的目录路径，如果用户取消选择，则返回None。
+
+    """
+    return compat.getexistingdirectory(parent, title, start_dir) or None
 
 
 def get_existing_directory_url(
@@ -20,6 +39,18 @@ def get_existing_directory_url(
     start_dir: Optional[QUrl] = None,
     supported_schemes: Optional[List[str]] = None,
 ) -> Optional[QUrl]:
+    """
+    弹出选择目录URL对话框，获取已存在的目录URL
+
+    Args:
+        parent: 父窗口
+        title:  对话框标题
+        start_dir: 起始目录
+        supported_schemes: 支持的URL协议
+
+    Returns:
+        用户选择的目录URL，如果用户取消选择，则返回None
+    """
     if start_dir is None:
         start_dir = QUrl()
     if not supported_schemes:
@@ -46,7 +77,18 @@ def get_open_file(
     start_dir: str = "",
     filters: str = "",
 ) -> Optional[str]:
-    # filename, _ = QFileDialog.getOpenFileName(parent, title, start_dir, filters)
+    """
+    弹出选择文件对话框，获取已存在的文件路径。
+
+    Args:
+        parent: 父窗口
+        title:  对话框标题
+        start_dir: 起始目录
+        filters: 文件过滤器
+
+    Returns:
+        用户选择的文件路径，如果用户取消选择，则返回None。
+    """
     filename, _ = compat.getopenfilename(parent, title, start_dir, filters)
     return filename or None
 
@@ -57,7 +99,18 @@ def get_open_files(
     start_dir: str = "",
     filters: str = "",
 ) -> Optional[List[str]]:
-    # filenames, _ = QFileDialog.getOpenFileNames(parent, title, start_dir, filters)
+    """
+    弹出选择多个文件对话框，获取已存在的文件路径。
+
+    Args:
+        parent: 父窗口
+        title:  对话框标题
+        start_dir:  起始目录
+        filters:  文件过滤器
+
+    Returns:
+        用户选择的文件路径列表，如果用户取消选择，则返回None。
+    """
     filenames, _ = compat.getopenfilenames(parent, title, start_dir, filters)
     return filenames or None
 
@@ -68,6 +121,17 @@ def get_save_file(
     start_dir: str = "",
     filters: str = "",
 ) -> Optional[str]:
-    # filename, _ = QFileDialog.getSaveFileName(parent, title, start_dir, filters)
+    """
+    弹出保存文件对话框，获取保存文件的路径。
+
+    Args:
+        parent: 父窗口
+        title:  对话框标题
+        start_dir: 起始目录
+        filters: 文件过滤器
+
+    Returns:
+        用户选择的保存文件路径，如果用户取消选择，则返回None。
+    """
     filename, _ = compat.getsavefilename(parent, title, start_dir, filters)
     return filename or None
