3a982b3615ac7c3f3369608b8b589b3c415b47be
zimolab <zimolab@aliyun.com>
2024-10-20 17:51:33 +0800

    refactor: add docstrings and move uprint() and clear_output() to uoutput.py

--- pyguiadapter/adapter/ucontext.py
+++ pyguiadapter/adapter/ucontext.py
@@ -1,3 +1,11 @@
+"""
+@Time    : 2024.10.20
+@File    : ucontext.py
+@Author  : zimolab
+@Project : PyGUIAdapter
+@Desc    : 维护全局上下文信息，提供一些核心功能
+"""
+
 import warnings
 from concurrent.futures import Future
 from typing import Any, Type, Optional, Callable, Dict
@@ -6,7 +14,9 @@ from qtpy.QtCore import QObject, Signal, QMutex
 
 from ..utils import BaseCustomDialog
 from ..utils.messagebox import show_messagebox
-from ..windows.fnexec import BaseFnExecuteWindow
+
+# noinspection PyProtectedMember
+from ..windows.fnexec._base import BaseFnExecuteWindow
 
 
 # noinspection PyMethodMayBeStatic,SpellCheckingInspection
@@ -209,48 +219,10 @@ def get_current_window() -> Optional[BaseFnExecuteWindow]:
 
 
 def is_function_cancelled() -> bool:
-    """检测函数是否被用户取消
+    """检测函数是否被用户取消。
 
     Returns:
-        用户发出取消函数执行的请求时返回`True`，否则返回`False`
+        检测到用户发出取消函数执行的请求时返回`True`，否则返回`False`
     """
     global _context
     return _context.is_function_cancelled()
-
-
-# noinspection SpellCheckingInspection
-def uprint(
-    *args: Any,
-    sep: str = " ",
-    end: str = "\n",
-    html: bool = False,
-    scroll_to_bottom: bool = True,
-) -> None:
-    """打印信息到输出浏览器
-
-    Args:
-        *args: 要打印的信息
-        sep: 每条信息之间的分隔字符串
-        end: 添加到最后一条信息后面的字符串
-        html: 要打印的信息是否为`html`格式
-        scroll_to_bottom: 打印信息后是否将`输出浏览器`滚动到最底部
-
-    Returns:
-        无返回值
-
-    """
-    global _context
-    text = sep.join([str(arg) for arg in args]) + end
-    # noinspection PyUnresolvedReferences
-    _context.sig_uprint.emit(text, html, scroll_to_bottom)
-
-
-def clear_output() -> None:
-    """清除`输出浏览器`中所有内容
-
-    Returns:
-        无返回值
-    """
-    global _context
-    # noinspection PyUnresolvedReferences
-    _context.sig_clear_output.emit()
