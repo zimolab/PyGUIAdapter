02209e220d51c009a54662e659e4ec804b1482f0
zimolab <zimolab@aliyun.com>
2024-10-20 17:53:02 +0800

    refactor: add docstrings

--- pyguiadapter/adapter/utoast.py
+++ pyguiadapter/adapter/utoast.py
@@ -1,8 +1,18 @@
+"""
+@Time    : 2024.10.20
+@File    : utoast.py
+@Author  : zimolab
+@Project : PyGUIAdapter
+@Desc    : 提供toast消息弹窗相关的功能
+"""
+
 from typing import Optional
 
 from .ucontext import _context
 from ..toast import ToastConfig
-from ..windows.fnexec import BaseFnExecuteWindow
+
+# noinspection PyProtectedMember
+from ..windows.fnexec._base import BaseFnExecuteWindow
 
 
 def _on_show_toast(
@@ -23,18 +33,36 @@ def _on_clear_toasts() -> None:
     wind = None
 
 
+_context.sig_show_toast.connect(_on_show_toast)
+_context.sig_clear_toasts.connect(_on_clear_toasts)
+
+
 def show_toast(
     message: str,
     duration: int = 3000,
     config: Optional[ToastConfig] = None,
     clear: bool = False,
 ) -> None:
+    """
+    显示toast消息
+
+    Args:
+        message: toast消息内容
+        duration: toast显示时间，单位为毫秒
+        config: toast配置
+        clear: 是否清除之前的toast消息
+
+    Returns:
+        无返回值
+    """
     _context.sig_show_toast.emit(message, duration, config, clear)
 
 
-def clear_toasts():
+def clear_toasts() -> None:
+    """
+    清除所有已发出的toast消息。
+
+    Returns:
+        无返回值
+    """
     _context.sig_clear_output.emit()
-
-
-_context.sig_show_toast.connect(_on_show_toast)
-_context.sig_clear_toasts.connect(_on_clear_toasts)
