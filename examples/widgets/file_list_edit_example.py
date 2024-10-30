0c40e733a2513f5b489a40d57c76d24837db7bfd
zimolab <zimolab@aliyun.com>
2024-10-30 17:59:40 +0800

    refactor: update example code

--- examples/widgets/file_list_edit_example.py
+++ examples/widgets/file_list_edit_example.py
@@ -1,20 +1,22 @@
 from pyguiadapter.adapter import GUIAdapter
 from pyguiadapter.adapter.uoutput import uprint
-from pyguiadapter.extend_types import file_list_t
+from pyguiadapter.extend_types import path_list_t, file_list_t, dir_list_t
 
 
-def file_list_edit_example(arg1: file_list_t, arg2: file_list_t):
+def file_list_edit_example(arg1: path_list_t, arg2: file_list_t, arg3: dir_list_t):
     """
-    This is an example for **file_list_t** type hint and **FileListEdit** widget.
+    This is an example for **path_list_t** type hint and **PathListEdit** widget.
 
     Args:
         arg1: description of arg1
         arg2: description of arg2
+        arg3: description of arg3
 
     Returns:
     """
     uprint(arg1)
     uprint(arg2)
+    uprint(arg3)
 
 
 if __name__ == "__main__":
