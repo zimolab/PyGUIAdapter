fc1cc68216ffe1531d6da0a09661ccc3cfeb86dd
zimolab <zimolab@aliyun.com>
2024-12-20 19:29:06 +0800

    docs(progress-dialog-feature): update function docstrings

--- pyguiadapter/adapter/uprogress.py
+++ pyguiadapter/adapter/uprogress.py
@@ -88,7 +88,6 @@ def show_progress_dialog(
     max_value: int = 100,
     inverted_appearance: bool = False,
     *,
-    title: str = "Progress",
     message_visible: bool = False,
     message_format: str = "%p%",
     message_centered: str = True,
@@ -98,12 +97,28 @@ def show_progress_dialog(
         "richtext", "markdown", "plaintext", "autotext"
     ] = "autotext",
     initial_info: str = "",
+    title: str = "Progress",
     size: tuple = (400, 150),
     modal: bool = True,
 ) -> None:
     """
-    显示进度对话框
+    显示进度对话框。开发者必须手动调用此函数来显示进度对话框，此函数除了用于显示进度对话框，
+    还用于对进度对话框进行配置。
 
+    Args:
+        min_value: 最小进度值，默认为`0`
+        max_value: 最大进度值，默认为`100`
+        inverted_appearance: 是否改变进度条的显示方式，使其以反方向显示进度
+        message_visible: 是否显示message信息
+        message_format: message信息的格式，支持：`%p%`（显示完成的百分比，这是默认显示方式）、`%v`（显示当前的进度值）和`%m`（显示总的步进值）
+        message_centered: message信息是否居中显示
+        info_visible: 是否显示info信息
+        info_centered: info信息是否居中显示
+        info_text_format: info信息的文本格式，支持`"richtext"` 、` "markdown"` 、`"plaintext"` 、`"autotext"`
+        initial_info: info信息的初始值
+        title: 对话框标题
+        size: 对话框大小
+        modal: 是否为模态对话框
     Returns:
         无返回值
     """
@@ -128,7 +143,7 @@ def show_progress_dialog(
 
 def dismiss_progress_dialog() -> None:
     """
-    隐藏进度对话框
+    关闭进度对话框
 
     Returns:
         无返回值
