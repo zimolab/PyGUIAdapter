bc41522f65694ff6f156bca25f1f47e9dba82e9c
zimolab <zimolab@aliyun.com>
2024-12-22 17:54:44 +0800

    build: remove old tools code and their dependencies

--- tools/make_table.py
+++
@@ -1,8 +0,0 @@
-from tools.gen_widget_doc import dataclass2table
-
-from pyguiadapter.windows.fnselect import FnSelectWindowConfig
-from pyguiadapter.windows import DocumentBrowserConfig
-
-if __name__ == "__main__":
-    # print(dataclass2table(FnSelectWindowConfig))
-    print(dataclass2table(DocumentBrowserConfig))
