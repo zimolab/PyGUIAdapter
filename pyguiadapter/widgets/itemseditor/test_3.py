5055c2671f6d03bce566d0ff376e6d11ec2cee84
zimolab <zimolab@aliyun.com>
2024-12-29 22:54:41 +0800

    chore: update test code

--- pyguiadapter/widgets/itemseditor/test_3.py
+++ pyguiadapter/widgets/itemseditor/test_3.py
@@ -1,33 +1,35 @@
-# from qtpy.QtWidgets import QApplication
-#
-# from pyguiadapter.widgets.itemseditor.object_tableview.valuetypes import (
-#     StringValue,
-#     IntValue,
-#     BoolValue,
-# )
-# from pyguiadapter.widgets.itemseditor.multiobject_editor import (
-#     MultiObjectEditor,
-#     MultiObjectEditorConfig,
-# )
-#
-# schema = {
-#     "Name": StringValue(),
-#     "Age": IntValue(),
-#     "Is Student": BoolValue(),
-#     "Occupation": StringValue(),
-# }
-#
-# app = QApplication([])
-# config = MultiObjectEditorConfig()
-# editor = MultiObjectEditor(None, schema, config)
-# editor.set_objects(
-#     [
-#         {"Name": "John", "Age": 25, "Is Student": True, "Occupation": "Engineer"},
-#         {"Name": "Jane", "Age": 30, "Is Student": False, "Occupation": "Teacher"},
-#         {"Name": "Bob", "Age": 40, "Is Student": True, "Occupation": "Doctor"},
-#         {"Name": "Tom"},
-#     ]
-# )
-# editor.show()
-# app.exec_()
-# print(editor.get_objects())
+from qtpy.QtWidgets import QApplication
+
+from pyguiadapter.widgets.itemseditor.object_tableview.valuetypes import (
+    StringValue,
+    IntValue,
+    BoolValue,
+    FloatValue,
+)
+from pyguiadapter.widgets.itemseditor.multiobject_editor import (
+    MultiObjectEditor,
+    MultiObjectEditorConfig,
+)
+
+schema = {
+    "Name": StringValue(),
+    "Age": IntValue(),
+    "Is Student": BoolValue(),
+    "Occupation": StringValue(),
+    "Height": FloatValue(180.0),
+}
+
+app = QApplication([])
+config = MultiObjectEditorConfig()
+editor = MultiObjectEditor(None, schema, config)
+editor.set_objects(
+    [
+        {"Name": "John", "Age": 25, "Is Student": True, "Occupation": "Engineer"},
+        {"Name": "Jane", "Age": 30, "Is Student": False, "Occupation": "Teacher"},
+        {"Name": "Bob", "Age": 40, "Is Student": True, "Occupation": "Doctor"},
+        {"Name": "Tom"},
+    ]
+)
+editor.show()
+app.exec_()
+print(editor.get_objects())
