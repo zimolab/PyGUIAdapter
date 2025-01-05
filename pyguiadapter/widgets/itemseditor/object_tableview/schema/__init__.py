c6909e2c77983ee718d352d194439cc87e9e37fb
zimolab <zimolab@aliyun.com>
2025-01-05 15:56:58 +0800

    refactor: remove unused code

--- pyguiadapter/widgets/itemseditor/object_tableview/schema/__init__.py
+++ pyguiadapter/widgets/itemseditor/object_tableview/schema/__init__.py
@@ -1,10 +1,4 @@
 from ._value_type import ValueType
 from ._widget_mixin import ValueWidgetMixin, CellWidgetMixin
-from ._factory import ObjectSchemaFactory
 
-__all__ = [
-    "ValueType",
-    "ValueWidgetMixin",
-    "ObjectSchemaFactory",
-    "CellWidgetMixin",
-]
+__all__ = ["ValueType", "ValueWidgetMixin", "CellWidgetMixin"]
